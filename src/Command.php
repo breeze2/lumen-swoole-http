7ec5688f0d8145e701f96886f218e93bab370a87
breezelin <805511939@qq.com>
2017-11-04 14:19:18 +0800

    [*] command
    command

--- src/Command.php
+++ src/Command.php
@@ -4,21 +4,115 @@ namespace BL\SwooleHttp;
 class Command
 {
     const VERSION = 'lumen-swoole-http 0.1.0';
+    const CONFIG_PREFIX = 'SWOOLE_HTTP_';
 
-    
+    protected $lumen;
+    protected $config;
+    protected $pidFile;
+    protected $service;
 
-    public static function main($argv)
+    private function __construct()
     {
-        $command = new static();
-        return $command->run($argv);
+        $this->lumen = require dirname(LUMENSWOOLEHTTP_COMPOSER_INSTALL) . '/../bootstrap/swoole.php';
+        $this->config = $this->initializeConfig();
+        $this->pidFile = $this->config['pid_file'];
+
+    }
+
+    private function initializeConfig()
+    {
+        $params = array(
+            'reactor_num',
+            'worker_num',
+            'max_request',
+            // 'max_conn',
+            'task_worker_num',
+            'task_ipc_mode',
+            'task_max_request',
+            'task_tmpdir',
+            'dispatch_mode',
+            'dispatch_func',
+            'message_queue_key',
+            // 'daemonize',
+            'backlog',
+            // 'log_file',
+            'log_level',
+            'heartbeat_check_interval',
+            'heartbeat_idle_time',
+            'open_eof_check',
+            'open_eof_split',
+            'package_eof',
+            'open_length_check',
+            'package_length_type',
+            'package_length_func',
+            'package_max_length',
+            'open_cpu_affinity',
+            'cpu_affinity_ignore',
+            'open_tcp_nodelay',
+            'tcp_defer_accept',
+            'ssl_cert_file',
+            'ssl_method',
+            'ssl_ciphers',
+            'user',
+            'group',
+            'chroot',
+            'pid_file',
+            'pipe_buffer_size',
+            'buffer_output_size',
+            'socket_buffer_size',
+            'enable_unsafe_event',
+            'discard_timeout_request',
+            'enable_reuse_port',
+            'ssl_ciphers',
+            'enable_delay_receive',
+            'open_http_protocol',
+            'open_http2_protocol',
+            'open_websocket_protocol',
+            'open_mqtt_protocol',
+            'reload_async',
+            'tcp_fastopen',
+        );
+        $settings = array();
+        foreach ($params as $param) {
+            $key = self::CONFIG_PREFIX . strtoupper($param);
+            $value = env($key);
+            if ($value !== null) {
+                $settings[$param] = $value;
+            }
+        }
+
+        $config['worker_num'] = env(self::CONFIG_PREFIX . 'WORK_NUM', 1);
+        $config['max_conn'] = env(self::CONFIG_PREFIX . 'MAX_CONN', 2000);
+        $config['daemonize'] = env(self::CONFIG_PREFIX . 'DAEMONIZE', false);
+        $config['log_file'] = env(self::CONFIG_PREFIX . 'LOG_FILE', storage_path('logs/swoole-http.log'));
+
+
+        $config['swoole_settings'] = $settings;
+        $config['host'] = env(self::CONFIG_PREFIX . 'HOST', '127.0.0.1');
+        $config['port'] = env(self::CONFIG_PREFIX . 'PORT', 9050);
+        $config['gzip'] = env(self::CONFIG_PREFIX . 'GZIP', true);
+        $config['gzip_min_length'] = env(self::CONFIG_PREFIX . 'GZIP_MIN_LENGTH', 1024);
+        $config['deal_with_public'] = env(self::CONFIG_PREFIX . 'DEAL_WITH_PUBLIC', false);
+        $config['pid_file'] = env(self::CONFIG_PREFIX . 'PID_FILE', storage_path('app/swoole-http.pid'));
+        $config['root_dir'] = base_path();
+        return $config;
     }
 
     protected function run($argv)
     {
         switch ($argv[1]) {
             case 'start':
-
+                $this->startService();
                 break;
+
+            case 'stop':
+                $this->stopService();
+            
+            case 'restart':
+                $this->restartService();
+            
+            case 'reload':
+                $this->reloadService();
 
             default:
                 # code...
@@ -26,17 +120,58 @@ class Command
         }
     }
 
+    protected function startService()
+    {
+        if ($this->getPid()) {
+            echo 'umen-swoole-http is already running' . PHP_EOL;
+            exit(1);
+        }
+
+        $service = new Service($this->lumen, $this->config);
+        $service->start();
+    }
+
+    protected function restartService()
+    {
+        echo storage_path('app');
+    }
+
+    protected function reloadService()
+    {
+        echo storage_path('app');
+    }
+
+    protected function stopService()
+    {
+        echo storage_path('app');
+    }
+
+    protected function sendSignal($signal)
+    {
+        if ($pid = $this->getPid()) {
+            posix_kill($pid, $sig);
+        } else {
+            echo "umen-swoole-http is not running!" . PHP_EOL;
+            exit(1);
+        }
+    }
+
     protected function getPid()
     {
-        $pid_file = getPidFile();
-        if (file_exists($pid_file)) {
-            $pid = file_get_contents($pid_file);
+        if ($this->pidFile && file_exists($this->pidFile)) {
+            $pid = file_get_contents($this->pidFile);
             if (posix_getpgid($pid)) {
                 return $pid;
             } else {
-                unlink($pid_file);
+                unlink($this->pidFile);
             }
         }
         return false;
+    }
+
+    public static function main($argv)
+    {
+        $command = new static;
+        return $command->run($argv);
     }
 }
--- src/Service.php
+++ src/Service.php
@@ -1,8 +1,156 @@
 <?php
 namespace BL\SwooleHttp;
 
+use Illuminate\Http\Request as IlluminateHttpRequest;
+use Illuminate\Http\Response as IlluminateHttpResponse;
+use swoole_http_server as SwooleHttpServer;
+
 class Service
 {
-	
+
+    protected $app;
+    protected $pidFile;
+    protected $server;
+    // protected $host;
+    // protected $port;
+    // protected $settings;
+
+    public function __construct(Application $app, $config)
+    {
+        $this->app = $app;
+        $this->pidFile = $config['pid_file'];
+        $this->server  = new SwooleHttpServer($config['host'], $config['port']);
+        if (isset($config['swoole_settings']) && !empty($config['swoole_settings'])) {
+            $this->server->set($config['swoole_settings']);
+        }
+    }
+
+    public function start()
+    {
+        $this->server->on('start', array($this, 'onStart'));
+        $this->server->on('shutdown', array($this, 'onShutdown'));
+        $this->server->on('WorkerStart', array($this, 'onWorkerStart'));
+        $this->server->on('request', array($this, 'onRequest'));
+
+        // require __DIR__ . '/mime.php';
+
+        $this->server->start();
+    }
+
+    public function onStart($serv)
+    {
+        file_put_contents(
+            $this->pidFile,
+            $serv->master_pid
+        );
+    }
+
+    public function onShutdown()
+    {
+        unlink($this->pidFile);
+    }
+
+    public function onWorkerStart($serv, $worker_id)
+    {
+
+    }
+
+    public function onRequest($request, $response)
+    {
+        try {
+            $http_request = $this->parseRequest($request);
+            $http_response = $this->app->dispatch($http_request);
+
+            // Is gzip enabled and the client accept it?
+            $accept_gzip = 1 && isset($request->header['accept-encoding']) && stripos($request->header['accept-encoding'], 'gzip') !== false;
+
+            if ($http_response instanceof IlluminateHttpResponse) {
+                $this->makeResponse($response, $http_response, $accept_gzip);
+            } else {
+                //echo (string) $response;
+                $response->end((string)$http_response);
+            }
+
+        } catch (ErrorException $e) {
+            if (strncmp($e->getMessage(), 'swoole_', 7) === 0) {
+                fwrite(STDOUT, $e->getFile() . '(' . $e->getLine() . '): ' . $e->getMessage() . PHP_EOL);
+            }
+        } finally {
+            if (count($this->app->getMiddleware()) > 0) {
+                $this->app->callTerminableMiddleware($http_response);
+            }
+        }
+    }
+
+    protected function parseRequest($request)
+    {
+        $get = isset($request->get) ? $request->get : array();
+        $post = isset($request->post) ? $request->post : array();
+        $cookie = isset($request->cookie) ? $request->cookie : array();
+        $server = isset($request->server) ? $request->server : array();
+        $header = isset($request->header) ? $request->header : array();
+        $files = isset($request->files) ? $request->files : array();
+        $fastcgi = array();
+
+        // merge headers into server which are filtered ed by swoole
+        // make a new array when php 7 has different behavior on foreach
+        $new_header = array();
+        foreach ($header as $key => $value) {
+            $new_header['http_' . $key] = $value;
+        }
+        $server = array_merge($server, $new_header);
+
+        $new_server = array();
+        // swoole has changed all keys to lower case
+        foreach ($server as $key => $value) {
+            $new_server[strtoupper($key)] = $value;
+        }
+
+        // override $_SERVER, for many packages use the raw variable
+        // $_SERVER = array_merge($this->_SERVER, $new_server);
+
+        $content = $request->rawContent() ?: null;
+
+        $http_request = new IlluminateHttpRequest($get, $post, $fastcgi, $cookie, $files, $new_server, $content);
+
+        return $http_request;
+    }
+
+    protected function makeResponse($response, $http_response, $accept_gzip)
+    {
+        // status
+        $response->status($http_response->getStatusCode());
+        // headers
+        foreach ($http_response->headers->allPreserveCase() as $name => $values) {
+            foreach ($values as $value) {
+                $response->header($name, $value);
+            }
+        }
+        // cookies
+        foreach ($http_response->headers->getCookies() as $cookie) {
+            $response->rawcookie(
+                $cookie->getName(),
+                $cookie->getValue(),
+                $cookie->getExpiresTime(),
+                $cookie->getPath(),
+                $cookie->getDomain(),
+                $cookie->isSecure(),
+                $cookie->isHttpOnly()
+            );
+        }
+        // content
+        $content = $http_response->getContent();
+
+        // check gzip
+        if($accept_gzip && isset($response->header['Content-Type'])) {
+            $mime = $response->header['Content-Type'];
+            if(strlen($content) > $this->gzip_min_length && is_mime_gzip($mime)) {
+                $response->gzip($this->gzip);
+            }
+        }
+
+        // send content & close
+        $response->end($content);
+    }
 
 }
