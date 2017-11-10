6f661a760efead0bc45b8ac06edbac6e6998de84
breezelin <805511939@qq.com>
2017-11-10 16:34:28 +0800

    [*] auto reload
    auto reload

--- src/AutoReload.php
+++ src/AutoReload.php
@@ -16,8 +16,8 @@ class AutoReload
     protected $inotify;
     protected $pid;
     protected $reloadFileTypes = array('.php' => true);
-    protected $watchFiles = array();
-    protected $afterNSeconds = 10;
+    protected $watchFiles      = array();
+    protected $afterNSeconds   = 10;
     /**
      * 正在reload
      */
@@ -28,49 +28,41 @@ class AutoReload
      * @var array
      */
     protected $rootDirs = array();
-    function putLog($log)
+    public function putLog($log)
     {
-        $_log = "[".date('Y-m-d H:i:s')."]\t".$log."\n";
+        $_log = "[" . date('Y-m-d H:i:s') . "]\t" . $log . "\n";
         echo $_log;
     }
     /**
      * @param $serverPid
      * @throws NotFound
      */
-    function __construct($serverPid)
+    public function __construct($serverPid)
     {
         $this->pid = $serverPid;
-        if (posix_kill($serverPid, 0) === false)
-        {
+        if (posix_kill($serverPid, 0) === false) {
             throw new NotFound("Process#$serverPid not found.");
         }
         $this->inotify = inotify_init();
-        $this->events = IN_MODIFY | IN_DELETE | IN_CREATE | IN_MOVE;
+        $this->events  = IN_MODIFY | IN_DELETE | IN_CREATE | IN_MOVE;
         swoole_event_add($this->inotify, function ($ifd) {
             $events = inotify_read($this->inotify);
-            if (!$events)
-            {
+            if (!$events) {
                 return;
             }
             var_dump($events);
-            foreach($events as $ev)
-            {
-                if ($ev['mask'] == IN_IGNORED)
-                {
+            foreach ($events as $ev) {
+                if ($ev['mask'] == IN_IGNORED) {
                     continue;
-                }
-                else if ($ev['mask'] == IN_CREATE or $ev['mask'] == IN_DELETE or $ev['mask'] == IN_MODIFY or $ev['mask'] == IN_MOVED_TO or $ev['mask'] == IN_MOVED_FROM)
-                {
+                } else if ($ev['mask'] == IN_CREATE or $ev['mask'] == IN_DELETE or $ev['mask'] == IN_MODIFY or $ev['mask'] == IN_MOVED_TO or $ev['mask'] == IN_MOVED_FROM) {
                     $fileType = strrchr($ev['name'], '.');
                     //非重启类型
-                    if (!isset($this->reloadFileTypes[$fileType]))
-                    {
+                    if (!isset($this->reloadFileTypes[$fileType])) {
                         continue;
                     }
                 }
                 //正在reload，不再接受任何事件，冻结10秒
-                if (!$this->reloading)
-                {
+                if (!$this->reloading) {
                     $this->putLog("after 10 seconds reload the server");
                     //有事件发生了，进行重启
                     swoole_timer_after($this->afterNSeconds * 1000, array($this, 'reload'));
@@ -79,7 +71,7 @@ class AutoReload
             }
         });
     }
-    function reload()
+    public function reload()
     {
         $this->putLog("reloading");
         //向主进程发送信号
@@ -87,8 +79,7 @@ class AutoReload
         //清理所有监听
         $this->clearWatch();
         //重新监听
-        foreach($this->rootDirs as $root)
-        {
+        foreach ($this->rootDirs as $root) {
             $this->watch($root);
         }
         //继续进行reload
@@ -98,26 +89,25 @@ class AutoReload
      * 添加文件类型
      * @param $type
      */
-    function addFileType($type)
+    public function addFileType($type)
     {
-        $type = trim($type, '.');
+        $type                               = trim($type, '.');
         $this->reloadFileTypes['.' . $type] = true;
     }
     /**
      * 添加事件
      * @param $inotifyEvent
      */
-    function addEvent($inotifyEvent)
+    public function addEvent($inotifyEvent)
     {
         $this->events |= $inotifyEvent;
     }
     /**
      * 清理所有inotify监听
      */
-    function clearWatch()
+    public function clearWatch()
     {
-        foreach($this->watchFiles as $wd)
-        {
+        foreach ($this->watchFiles as $wd) {
             inotify_rm_watch($this->inotify, $wd);
         }
         $this->watchFiles = array();
@@ -128,49 +118,42 @@ class AutoReload
      * @return bool
      * @throws NotFound
      */
-    function watch($dir, $root = true)
+    public function watch($dir, $root = true)
     {
         //目录不存在
-        if (!is_dir($dir))
-        {
+        if (!is_dir($dir)) {
             throw new NotFound("[$dir] is not a directory.");
         }
         //避免重复监听
-        if (isset($this->watchFiles[$dir]))
-        {
+        if (isset($this->watchFiles[$dir])) {
             return false;
         }
         //根目录
-        if ($root)
-        {
+        if ($root) {
             $this->rootDirs[] = $dir;
         }
-        $wd = inotify_add_watch($this->inotify, $dir, $this->events);
+        $wd                     = inotify_add_watch($this->inotify, $dir, $this->events);
         $this->watchFiles[$dir] = $wd;
-        $files = scandir($dir);
-        foreach ($files as $f)
-        {
-            if ($f == '.' or $f == '..')
-            {
+        $files                  = scandir($dir);
+        foreach ($files as $f) {
+            if ($f == '.' or $f == '..') {
                 continue;
             }
             $path = $dir . '/' . $f;
             //递归目录
-            if (is_dir($path))
-            {
+            if (is_dir($path)) {
                 $this->watch($path, false);
             }
             //检测文件类型
             $fileType = strrchr($f, '.');
-            if (isset($this->reloadFileTypes[$fileType]))
-            {
-                $wd = inotify_add_watch($this->inotify, $path, $this->events);
+            if (isset($this->reloadFileTypes[$fileType])) {
+                $wd                      = inotify_add_watch($this->inotify, $path, $this->events);
                 $this->watchFiles[$path] = $wd;
             }
         }
         return true;
     }
-    function run()
+    public function run()
     {
         swoole_event_wait();
     }
